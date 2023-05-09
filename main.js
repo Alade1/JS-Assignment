4028c5e23ca4dd20b575bd5f38c295ebb0f8f154
Alade1 <aladefolarin7@gmail.com>
2023-05-08 13:51:10 -1100

    This is JS assignment

---
+++ main.js
@@ -0,0 +1,61 @@
+function alertUser1(event){
+    event.preventDefault();
+    let valueOne = document.getElementById("valueOne").value;
+    let valueTwo =document.getElementById("valueTwo").value;
+    let answer = document.getElementById('answer');
+    let result = Number(valueOne) + Number(valueTwo);
+    answer.innerHTML = result;
+    console.log(result);
+
+}
+
+function alertUser2(event){
+    event.preventDefault();
+    let valueOne = document.getElementById("valueOne").value;
+    let valueTwo =document.getElementById("valueTwo").value;
+    let answer = document.getElementById('answer');
+    let result = Number(valueOne) ** Number(valueTwo);
+    answer.innerHTML=result;
+    console.log(result)
+
+}
+function alertUser3(event){
+    event.preventDefault();
+    let valueOne = document.getElementById("valueOne").value;
+    let valueTwo =document.getElementById("valueTwo").value;
+    let answer = document.getElementById('answer');
+    let result = Number(valueOne) * Number(valueTwo);
+    answer.innerHTML=result;
+    console.log(result)
+
+}
+function alertUser4(event){
+    event.preventDefault();
+    let valueOne = document.getElementById("valueOne").value;
+    let valueTwo =document.getElementById("valueTwo").value;
+    let answer = document.getElementById('answer');
+    let result = Number(valueOne) / Number(valueTwo);
+    answer.innerHTML=result;
+    console.log(result)
+
+}
+function alertUser5(event){
+    event.preventDefault();
+    let valueOne = document.getElementById("valueOne").value;
+    let valueTwo =document.getElementById("valueTwo").value;
+    let answer = document.getElementById('answer');
+    let result = Number(valueOne) - Number(valueTwo);
+    answer.innerHTML=result;
+    console.log(result)
+
+}
+function alertUser6(event){
+    event.preventDefault();
+    let valueOne = document.getElementById("valueOne").value;
+    let valueTwo =document.getElementById("valueTwo").value;
+    let answer = document.getElementById('answer');
+    let result = Number(valueOne) % Number(valueTwo);
+    answer.innerHTML=result;
+    console.log(result)
+
+}
