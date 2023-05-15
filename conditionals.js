b1840e587d5b8b72249716104d9088fbc8c097b0
Alade1 <aladefolarin7@gmail.com>
2023-05-14 13:24:29 -1100

    loop

---
+++ conditionals.js
@@ -0,0 +1,25 @@
+let isRaining = false;
+
+if(isRaining){
+    console.log("bring a raincoat");
+}
+else{
+    console.log("No need for raincoat");
+}
+
+let age = 18;
+ 
+if (age === 18){
+    console.log("Can watch mojo");
+}
+else if(age <18){
+    console.log("Get out");
+}
+else{
+    console.log("More than enough");
+}
+
+
+
+
+
---
+++ loops.js
@@ -0,0 +1,19 @@
+const FullStackFebCohort = [];
+FullStackFebCohort.push("Dolapo");
+FullStackFebCohort.push("Folarin");
+FullStackFebCohort.push("Kelvin");
+FullStackFebCohort.push("Ifeanyi");
+FullStackFebCohort.push("Kelechi");
+console.log(FullStackFebCohort);
+
+FullStackFebCohort.push("Julius");
+console.log(FullStackFebCohort);
+
+FullStackFebCohort.pop("Julius");
+console.log(FullStackFebCohort);
+
+const FirstSetStudent = FullStackFebCohort.shift();
+console.log(FirstSetStudent);
+console.log(FullStackFebCohort);
+FullStackFebCohort.unshift("Julius");
+console.log(FullStackFebCohort);
